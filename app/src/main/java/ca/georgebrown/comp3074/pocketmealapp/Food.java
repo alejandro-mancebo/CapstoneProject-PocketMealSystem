e4f5caccac84ca48d5ab71de8117ff35756e72c1
Xris4o0 <xris4o0@gmail.com>
2020-03-07 14:53:57 -0500

    Clean Up Again

--- app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/Food.java
+++ app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/Food.java
@@ -2,36 +2,35 @@ package ca.georgebrown.comp3074.pocketmealapp;
 
 public class Food {
 
+    private String foodname;
+    private String category;
+    private String expiry_date;
+    private String ingredients;
+    private String email;
 
     public String getFoodname() {
         return foodname;
     }
-
     public void setFoodname(String foodname) {
         this.foodname = foodname;
     }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+    public String getExpiry_date() { return expiry_date; }
+    public void setExpiry_date(String expiry_date) { this.expiry_date = expiry_date; }
+    public String getIngredients() { return ingredients; }
+    public void setIngredients(String ingredients) { this.ingredients = ingredients; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    private String foodname;
-        private String category;
-        private String expiry_date;
-        private String ingredients;
+    public Food(String category, String expiry_date, String ingredients) {
 
+        //this.food_name = food_name;
+        this.category = category;
+        this.expiry_date = expiry_date;
+        this.ingredients = ingredients;
 
-
-    private String email;
-
-        public Food(
-                        String category,
-                        String expiry_date,
-                        String ingredients) {
-
-            //this.food_name = food_name;
-            this.category = category;
-            this.expiry_date = expiry_date;
-            this.ingredients = ingredients;
-
-
-        }
+    }
 
     public Food(String foodname,
             String category,
@@ -46,10 +45,7 @@ public class Food {
 
     }
 
-    public Food(String foodname,
-                String category,
-                String expiry_date,
-                String ingredients,String email) {
+    public Food(String foodname, String category, String expiry_date, String ingredients, String email) {
 
         this.foodname = foodname;
         this.category = category;
@@ -60,39 +56,8 @@ public class Food {
     }
 
 
-        public String getCategory() {
-            return category;
-        }
-
-        public void setCategory(String category) {
-            this.category = category;
-        }
-
-        public String getExpiry_date() {
-            return expiry_date;
-        }
-
-        public void setExpiry_date(String expiry_date) {
-            this.expiry_date = expiry_date;
-        }
-
-        public String getIngredients() {
-            return ingredients;
-        }
-
-        public void setIngredients(String ingredients) {
-            this.ingredients = ingredients;
-        }
-    public String getEmail() {
-        return email;
+    public String toString() {
+        return  "     Category :" + category + "     Expiry Date :" + expiry_date +"     Ingredient:"+ingredients + "\n";
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-        public String toString() {
-            return  "     Category :" + category + "     Expiry Date :" + expiry_date +"     Ingredient:"+ingredients + "\n";
-        }
 
 }
--- app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/Point.java
+++ app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/Point.java
@@ -2,28 +2,23 @@ package ca.georgebrown.comp3074.pocketmealapp;
 
 public class Point {
 
+    private double longitude,latitude;
+
     public double getLongitude() {
         return longitude;
     }
-
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
-
     public double getLatitude() {
         return latitude;
     }
-
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
-    private double longitude,latitude;
-
-   public Point(double lon, double lat){
-
+    public Point(double lon, double lat){
        longitude = lon;
        latitude = lat;
-
-   }
+    }
 }
