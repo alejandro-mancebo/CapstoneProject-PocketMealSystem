887d68ea5c4a7caec6f4e41eb3aa116366ba8bc5
Xris4o0 <xris4o0@gmail.com>
2020-03-13 13:34:05 -0400

    Add Food Comments
    - Added helpful comments to AddFoodFragment.

--- app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/ui/add_food/AddFoodFragment.java
+++ app/src/main/java/ca/georgebrown/comp3074/pocketmealapp/ui/add_food/AddFoodFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,6 +17,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import java.util.Date;
 
+import ca.georgebrown.comp3074.pocketmealapp.LoginActivity;
 import ca.georgebrown.comp3074.pocketmealapp.R;
 
 public class AddFoodFragment extends Fragment {
@@ -23,8 +25,8 @@ public class AddFoodFragment extends Fragment {
     private AddFoodViewModel addFoodViewModel;
 
     // UI Navigation Code Variables
-    private Date foodExp;
-    private EditText foodName, foodDisc, foodAllergy;
+    private Date foodExpDate;
+    private EditText foodName, foodDisc, foodAllergy, foodExp;
     private Button addFood;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -40,12 +42,28 @@ public class AddFoodFragment extends Fragment {
             }
         });
 
+        foodName = root.findViewById(R.id.foodnameEdit);
+        foodDisc = root.findViewById(R.id.foodDescEdit);
+        foodAllergy = root.findViewById(R.id.allergyEdit);
+        foodExp = root.findViewById(R.id.expDateEdit);
+
         addFood = root.findViewById(R.id.addFoodBTN);
 
         addFood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // What happens when someone clicks add food button.
+
+                // What happens when someone clicks Add Food button.
+                // Check Expiry Date provided by the user.
+                if (foodExp.getText().toString().matches("\\d{2}-\\d{2}-\\d{2}")) {
+
+                    // Create food object and place it inside database. Use the LoginActivity.currentUser.getDisplayName() to see what user it should go under.
+
+                } else {
+                    // Show error msg
+                    Toast.makeText(getContext(), "Please enter a valid expiry date using the format provided.", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
